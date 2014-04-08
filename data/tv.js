97d4b36a79e20334743762e35121d79b08ca4a41
jzm17173 <823458264@qq.com>
2014-04-08 20:40:48 +0800

    冰与火之歌:权力的游戏 第4季01 双剑

--- data/tv.js
+++ data/tv.js
@@ -4,6 +4,12 @@ Bear.island(document.getElementById('loveandpeace')).jsonpCallback(
     "area": ["韩国", "香港", "日本", "英国"],
     "list": [
         {
+            "name": "冰与火之歌:权力的游戏 第4季01 双剑",
+            "area": "美国",
+            "time": "2014/04/08",
+            "id": ""
+        },
+        {
             "name": "来自星星的你",
             "area": "韩国",
             "time": "2014/02/08",
